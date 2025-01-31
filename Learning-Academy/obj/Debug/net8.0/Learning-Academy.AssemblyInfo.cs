60ffd7b704d75cccba589c80092d47ed837b9e32
Marwa <mm9380072@gmail.com>
2025-01-31 07:08:54 +0200

    Merge Learning-Academy folder into Back-End

--- Learning-Academy/obj/Debug/net8.0/Learning-Academy.AssemblyInfo.cs
+++ Learning-Academy/obj/Debug/net8.0/Learning-Academy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning-Academy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e197541574235d81c243002dded846b7b028cf1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning-Academy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning-Academy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
