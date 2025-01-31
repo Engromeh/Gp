00628c5abd59d2bd1837e00e4b35cb774acfdf69
Marwa <mm9380072@gmail.com>
2025-01-31 06:44:55 +0200

    Initial commit for back project

---
+++ Learning-Academy/Models/Attachment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Learning_Academy.Models
+{
+    public class Attachment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Type { get; set; }=null!;
+        
+        public int Size { get; set; }
+        [ForeignKey("Massage")]
+        public virtual int MassageId { get; set; }
+        public virtual Massage Massage { get; set; } 
+
+
+    }
+}
