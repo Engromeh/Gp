e7dd91b8578c213eb22e67309eeaa6b1f2bd1fb7
Mahmoud Saaed Romeh <123378237+Engromeh@users.noreply.github.com>
2025-03-14 00:45:31 +0200

    Create imge in login and register

--- Back-end/Learning-Academy/obj/Debug/net8.0/Learning-Academy.AssemblyInfo.cs
+++ Back-end/Learning-Academy/obj/Debug/net8.0/Learning-Academy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning-Academy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01ecf6515f21edbb10e7ac2e8e2b13806f8ff90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59ab5486badfbfafee97df2d1a0900702c69e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning-Academy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning-Academy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
